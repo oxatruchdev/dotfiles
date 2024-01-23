7e3747d57731b635619df9b025400d7dad304fee
Oscar Sauceda <oscar@uledger.co>
2024-01-23 14:35:48 -0600

    add lsp-signature plugin

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,3 +4,9 @@ require("osmodiar16.lazy")
 require("osmodiar16.setup.telescope-setup")
 require("osmodiar16.setup.treesitter-setup")
 
+require("lsp_signature").setup({
+  bind = true, -- This is mandatory, otherwise border config won't get registered.
+  handler_opts = {
+    border = "rounded",
+  },
+})
