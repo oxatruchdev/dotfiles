6e33a7b4c02202a659630afa3d4df00d26fc7f1b
Oscar Sauceda <oscar@uledger.co>
2024-01-26 10:58:20 -0600

    add config for terraform

--- dot_config/nvim/lua/osmodiar16/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/osmodiar16/plugins/lsp/lspconfig.lua
@@ -178,5 +178,10 @@ return {
       capabilities = capabilities,
       on_attach = on_attach,
     })
+
+    lspconfig["terraformls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
   end,
 }
--- dot_config/nvim/lua/osmodiar16/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/osmodiar16/plugins/lsp/mason.lua
@@ -40,6 +40,7 @@ return {
         "clangd",
         "gopls",
         "templ",
+        "terraformls",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
